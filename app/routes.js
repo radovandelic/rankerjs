760f27fa3f49aa3776ec505ff30557eab3c4838a
pccfpp <filippo_piccardi@yahoo.it>
2017-10-19 21:44:55 +0200

    routes set

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,38 @@
 var express = require("express");
 var router = express.Router();
-var model = require("./model");
+var Image = require("./model");
 var bodyParser = require("body-parser");
 var db = require("./database");
 
 db.startDB();
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
+router.get("/", (req, res) => {
+    Image.find((err, images)=> {
+        if(err){
+            console.log(err);
+        }else {
+            res.render("index.ejs", { db: images });
+        }
+    })
+});
+
+router.get("/add", (req, res) => {
+    res.render("add.ejs");
+});
+
+router.post("/add", (req, res) => {
+    req.body.upvotes = 0;
+    req.body.downvotes = 0;
+    var pic = new Image(req.body);
+    pic.save((err) => {
+        if(err){
+            res.send(err);
+        } else {
+            res.render("add.ejs");
+        }
+    });
+});
 
