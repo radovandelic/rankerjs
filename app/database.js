4b8946873c183d7674070fbef25e2da273c8ecfe
Radovan Delic <infamouszgbgd@yahoo.com>
2017-10-30 15:10:47 +0100

    keys

--- app/database.js
+++ app/database.js
@@ -1,9 +1,12 @@
 var mongoose = require("mongoose");
-var keys = require("./keys");
+
+var keys = process.env.dbusername
+    ? { username: process.env.dbusername, password: process.env.dbpassword }
+    : require("./keys");
 
 module.exports = {
     database: `mongodb://${keys.username}:${keys.password}@ds121965.mlab.com:21965/student`,
-    startDB: function() {
+    startDB: function () {
         //to get rid of promise/deprecated warnings
         mongoose.Promise = global.Promise;
         mongoose.connect(this.database, { useMongoClient: true });
