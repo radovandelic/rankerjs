5768487ad91abf09fb137e9434401dad82fef0e8
Radovan Delic <infamouszgbgd@yahoo.com>
2017-11-04 15:03:05 +0100

    test img dl

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose")
 var ImgixClient = require('imgix-core-js');
 var apitoken = process.env.apitoken ? process.env.apitoken : require("./keys.js").apitoken;
 var client = new ImgixClient({
-    host: "resize.imgix.net",
+    host: "rankerjs.imgix.net",
     secureURLToken: apitoken
 });
 
